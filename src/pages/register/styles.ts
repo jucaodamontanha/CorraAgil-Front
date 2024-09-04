afe69f9596e18f72c1fedbea6750dee6f514024d
Karine <karinedeveloper@gmail.com>
2024-09-04 00:13:03 -0300

    feat: add component Register

---
+++ src/pages/register/styles.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#12263A",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  top: {
+    alignItems: "center",
+    marginBottom: 40,
+  },
+  title: {
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 32,
+  },
+  containerInput: {
+    marginTop: 30,
+    alignItems: "center",
+  },
+
+  forgetPassword: {
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 20,
+    marginBottom: 33,
+  },
+  containerButton: {
+    marginBottom: 96,
+  },
+});

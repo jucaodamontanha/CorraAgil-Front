b2fc8c8067c016ee9709c17d27c4d64502fe0be0
Wesley Marques <wesleytmarques@outlook.com>
2024-11-23 16:41:01 -0300

    feat/mudanças nas rotas do app

---
+++ app/changedPassword/style.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#12263A",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    image: {
+        marginBottom: 58
+    },
+    boxMid: {
+        width: 362,
+        gap: 20
+    },
+    textMid: {
+        color: "#FFF",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 27,
+        marginBottom: 58
+    }
+});
+
+export default styles;

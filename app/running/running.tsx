6e54301ac478f2dec6053001b2851105d357d8f6
Wesley Marques <wesleytmarques@outlook.com>
2025-01-27 22:18:51 -0300

    Feat: Inclusão do mapa com localização em real time do usuario

--- app/running/running.tsx
+++ app/running/running.tsx
@@ -1,9 +1,210 @@
 import { Image, StatusBar, Text, View } from "react-native"
+import { useEffect, useRef, useState } from "react";
 import styles from "./style"
 import { Button } from "../../src/components/Button"
-
+import { Entypo } from "@expo/vector-icons";
+import { BlurView } from 'expo-blur';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 export default function Running() {
+
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+    
+    const mapRef = useRef<MapView>(null);
+
+    async function requestLocationPermissions() {
+        const { granted } = await Location.requestForegroundPermissionsAsync();
+
+        if (granted) {
+            const currentPosition = await Location.getCurrentPositionAsync();
+            setLocation(currentPosition);
+
+        }
+    }   
+
+    useEffect( () => {
+        requestLocationPermissions();
+    }, [])
+
+    useEffect( () => {
+        Location.watchPositionAsync({
+            accuracy: Location.LocationAccuracy.Highest,
+            timeInterval: 1000,
+            distanceInterval: 1
+        }, (response) => {
+            setLocation(response);
+            mapRef.current?.animateCamera({
+                center: response.coords
+            })
+        })
+    },[])
+
+    const darkMapStyle = [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#242f3e"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#746855"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#242f3e"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.locality",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#263c3f"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#6b9a76"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#38414e"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#212a37"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9ca5b3"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#746855"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            {
+              "color": "#1f2835"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#f3d19c"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#2f3948"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#d59563"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#17263c"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#515c6d"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#17263c"
+            }
+          ]
+        }
+      ]
+
+
     return (
         <View style={styles.container}>
             <StatusBar backgroundColor="#12263A" barStyle="light-content" />
@@ -36,19 +237,37 @@ export default function Running() {
             </View>
 
             <View style={styles.boxBottom}>
-                <View style={styles.backgroundMap}>
 
-                </View>
+                {
+                    location &&
+
+                    <MapView
+                    ref={mapRef}
+                    style={styles.backgroundMap}
+                    initialRegion={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                        latitudeDelta: 0.005,
+                        longitudeDelta: 0.005
+                    }}
+                    showsUserLocation={true}
+                    customMapStyle={darkMapStyle}
+                    />
+                }
 
                 <View style={styles.transparentView}>
                     <Button title="CORRER" variant="primary" />
 
-                    <View style={styles.buttonMenu}>
-                        <Text style={{ color: "#FFA500" }}>Notificação</Text>
-                        <Text style={{ color: "#FFA500" }}>Correr</Text>
-                        <Text style={{ color: "#FFA500" }}>Timer</Text>
-                        <Text style={{ color: "#FFA500" }}>Histórico</Text>
-                    </View>
+                    <BlurView intensity={4} style={[styles.blurView, {backgroundColor: '#12263A'}]}>
+
+                        <View style={styles.buttonMenu}>
+                            <Text style={styles.text}>Notificação</Text>
+                            <Text style={styles.text}>Correr</Text>
+                            <Text style={styles.text}>Timer</Text>
+                            <Text style={styles.text}>Histórico</Text>
+                        </View>
+
+                    </BlurView>
                 </View>
             </View>
 
--- app/running/style.ts
+++ app/running/style.ts
@@ -1,88 +1,104 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: "#12263A",
         // justifyContent: "center",
         alignItems: "center",
-        
+
     },
-    boxTop:{
+    boxTop: {
         alignItems: "center"
     },
-    logo:{
+    logo: {
         width: 104,
         height: 20,
         marginTop: 16,
         marginBottom: 18,
         alignItems: "center"
     },
-    cronometro:{
+    cronometro: {
         marginBottom: 22,
         alignItems: "center"
     },
-    timer:{
+    timer: {
         color: "#FFA500",
         fontWeight: 700,
         fontSize: 72
     },
-    duration:{
+    duration: {
         color: "#FFA500",
         fontWeight: 400,
         fontSize: 16
     },
-    status:{
+    status: {
         width: 350,
         height: 67,
         flexDirection: "row",
         justifyContent: "space-between",
-        
-    },
-    distance:{
-        
-        
-    },
-    distanceKM:{
-        textAlign: "center",
-        color: "#FFA500",
-        fontWeight: 600,
-        fontSize: 32
-    },
-    calories:{
 
     },
-    caloriesQuant:{
-        textAlign: "center",
-        color: "#FFA500",
-        fontWeight: 600,
-        fontSize: 32
-    },
-    steps:{
+    distance: {
+
 
     },
-    stepsDistance:{
+    distanceKM: {
         textAlign: "center",
         color: "#FFA500",
         fontWeight: 600,
         fontSize: 32
     },
-    boxBottom:{
+    calories: {
+
+    },
+    caloriesQuant: {
+        textAlign: "center",
+        color: "#FFA500",
+        fontWeight: 600,
+        fontSize: 32
+    },
+    steps: {
+
+    },
+    stepsDistance: {
+        textAlign: "center",
+        color: "#FFA500",
+        fontWeight: 600,
+        fontSize: 32
+    },
+    boxBottom: {
         marginTop: 20
     },
-    backgroundMap:{
-        // position: "relative"
+    backgroundMap: {
+        width: 669,
+        height: 554,
+        position: "relative"
     },
-    transparentView:{
-        // position: "absolute"
-        marginTop: 385
+    transparentView: {
+        position: "absolute",
+        marginTop: 385,
+        marginLeft: 170
     },
-    buttonMenu:{
+    blurView: {
+        width: 334,
+        height: 66,
+        marginTop: 29,
+        borderRadius: 20,
+        overflow: 'hidden',
+    },
+    buttonMenu: {
         width: 335,
         height: 66,
         marginTop: 29,
         flexDirection: "row",
         justifyContent: "space-between",
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+    text:{
+        color: "#FFA500",
+        fontSize: 12,
     }
 })
 

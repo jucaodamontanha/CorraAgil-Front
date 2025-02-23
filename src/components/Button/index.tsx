439dfb894a4b0df4583899bb82f4f56d24fa1de3
Wesley Marques <wesleytmarques@outlook.com>
2025-02-23 17:56:26 -0300

    Feat: Inclusão do calculo da calorias, distancia percorrido, mapa preciso, tempo calculando.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import {
   TouchableOpacity,
   Text,
@@ -8,14 +8,76 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { styles } from "./styles";
+import { FontAwesome, Ionicons, MaterialIcons, Octicons, AntDesign } from "@expo/vector-icons";
 
-interface ButtonProps extends TouchableOpacityProps {
-  title: string;
-  variant?: "primary" | "secondary" | "tertiary";
-  iconName?: string;
-  loading?: boolean;
+type IconComponent = React.ComponentType<React.ComponentProps<typeof MaterialIcons>> |
+  React.ComponentType<React.ComponentProps<typeof FontAwesome>> |
+  React.ComponentType<React.ComponentProps<typeof Octicons>> |
+  React.ComponentType<React.ComponentProps<typeof Ionicons>> |
+  React.ComponentType<React.ComponentProps<typeof AntDesign>>;
+
+
+type Props = TouchableOpacityProps & {
+  title: string,
+  variant?: "primary" | "secondary" | "tertiary",
+  iconName?: string,
+  loading?: boolean,
+  IconCenter?: IconComponent,
+  IconCenterName?: String
 }
+// interface ButtonProps extends TouchableOpacityProps {
+//   title: string;
+//   variant?: "primary" | "secondary" | "tertiary";
+//   iconName?: string;
+//   loading?: boolean;
+//   IconCenter?: IconComponent;
+// }
 
+export const Button = forwardRef<React.ElementRef<typeof TouchableOpacity>, Props>(
+  ({ title, variant, iconName, loading, IconCenter, ...rest }, ref) => {
+
+    return (
+      <TouchableOpacity
+        ref={ref}
+        style={[
+          styles.button,
+          variant === "secondary" && styles.secondaryButton,
+          variant === "tertiary" && styles.tertiaryButton,
+        ]}
+        {...rest}
+      >
+        <View style={styles.content}>
+          {iconName && (
+            <Icon
+              name={iconName}
+              size={24}
+              color={variant === "secondary" ? "#fff" : "#000"}
+              style={styles.icon}
+            />
+          )}
+
+          {loading ?
+            (<ActivityIndicator color={"#000"} />)
+            : (
+              <Text
+                style={[
+                  styles.textButton,
+                  variant === "primary" && styles.textPrimary,
+                  variant === "secondary" && styles.textSecondary,
+                  variant === "tertiary" && styles.textTertiary,
+                ]}
+              >
+                {title}
+              </Text>
+            )}
+        </View>
+      </TouchableOpacity>
+
+    )
+  }
+)
+
+/*
 export function Button({
   title,
   variant = "primary",
@@ -58,3 +120,4 @@ export function Button({
     </TouchableOpacity>
   );
 }
+*/
